2e0bc52e79ced27353390f3c21f817ca985aa51d
NEW <zipi18k@gmail.com>
2019-11-08 10:21:00 +0200

    First commit

---
+++ BL_Project3/Class1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using DAL_Project3;
+
+namespace BL_Project3
+{
+    public static class Class1
+    {
+        public static SearchFilesDBEntities1 ent = new SearchFilesDBEntities1();
+
+        //יצירת רשימה שלתוכה יכנסו תוצאות החיפוש
+        public static List<string> fileList = new List<string>();
+
+        //פונקציה המקבלת מסלול לחיפוש + שם קובץ ומדפיסה את תוצאות החיפוש
+        public static List<string> DirSearch(string c, string word)
+
+        {
+
+            try
+            {
+
+
+                try
+                {
+                    // חיפוש תיקיות משנה בכל תיקיה
+                    foreach (string d in Directory.GetDirectories(c))
+                    {
+                        try
+                        {
+                            //ריצה על כל הקבצים בתוך התיקייה הנתונה
+                            foreach (string f in Directory.GetFiles(c, "*", SearchOption.AllDirectories))
+                            {
+                                // אם בשם הקובץ הנתון יש את מחרוזת החיפוש הנתונה- אז תוסיף אותו לרשימה + תדפיס אותו
+                                if (f.IndexOf(word, StringComparison.CurrentCultureIgnoreCase) > -1)
+                                {
+                                    fileList.Add(f);
+
+
+                                    Console.WriteLine(f);
+
+
+
+
+                                }
+
+
+                            }
+                        }
+                        catch (Exception)
+
+                        {
+
+                        }
+                        DirSearch(d, word);
+                    }
+
+                }
+
+                catch (Exception)
+                {
+                }
+
+            }
+
+            catch (Exception)
+            {
+                Console.WriteLine("ERROR");
+
+            }
+            return fileList;
+        }
+
+        public static void insertToDataBase(string fileName, string dirName, string result, DateTime startTime)
+        {
+            
+            ent.SearchFileTBL.Add(new SearchFileTBL
+            {
+                
+                FILE_NAME = fileName,
+                DIR_NAME= dirName,
+                DATE = startTime,
+                RESULT = result
+            });
+            ent.SaveChanges();
+            
+        }
+
+
+
+    }
+}
---
+++ UI_Project3/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BL_Project3;
+
+namespace UI_Project3
+{
+   public static class Program
+    {
+        static void Main(string[] args)
+        {
+
+          
+            void main2()
+            {
+
+
+
+
+                Console.WriteLine("MENU");
+                Console.WriteLine("1.Enter file name to search.");
+                Console.WriteLine("2.Enter file name to search + parent directory to search in.");
+                Console.WriteLine("3.Exit");
+                Console.WriteLine("--------------");
+                Console.WriteLine("enter your choise here:");
+                string option = Console.ReadLine();
+                    while (option != "1" && option != "2" && option != "3")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Error! Please chek 1 or 2 or 3");
+                        Console.ResetColor();
+                        Console.WriteLine("--------------");
+                        option = Console.ReadLine();
+
+
+                    }
+
+                switch (option)
+                {
+
+                    case "1":
+                        enter(option);
+                        break;
+
+                    case "2":
+                        enter(option);
+                        break;
+
+                    case "3":
+                        break;
+
+                    default:
+                        main2();
+                        break;
+
+
+
+                }
+
+
+
+            }
+            void enter(string option)
+            {
+
+
+
+                string file = string.Empty;
+                string dir = "d:\\";
+                DateTime startTime = DateTime.Now;
+
+
+                Console.WriteLine("Please enter file name:");
+                file = Console.ReadLine();
+                while (file == string.Empty)
+                {
+                    Console.WriteLine("Please enter file name:");
+                    file = Console.ReadLine();
+                }
+
+                if (option == "2")
+                {
+                    Console.WriteLine("Please enter directory name:");
+                    dir += Console.ReadLine();
+                    while (dir == "d:\\")
+                    {
+                        Console.WriteLine("Please enter directory name:");
+                        dir += Console.ReadLine();
+                    }
+
+                }
+                List<string> newFiles = Class1.DirSearch(dir, file);
+
+
+                if (newFiles.Count == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("file or directory is not found");
+                    Console.ResetColor();
+                    Class1.insertToDataBase(file, dir, "File Not Found",startTime);
+                }
+                foreach (var item in newFiles)
+                {
+                    Class1.insertToDataBase(file, dir,item, startTime);
+
+                }
+
+                Console.WriteLine("--------------end search--------------");
+
+                Console.WriteLine();
+                main2();
+
+                Console.ReadLine();
+
+            }
+            main2();
+        }
+    }
+        
+           
+}
+
